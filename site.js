9282915322d6bb11515eeedfdc65b5694ca6d5ff
Stephane Bruckert <contact@stephanebruckert.com>
2020-03-21 19:52:29 +0000

    Style

--- site.js
+++ site.js
@@ -1,30 +1,30 @@
 var o = {
-	loadData: () => {
-    $.getJSON(
-      // Generated in
-      // https://github.com/stephanebruckert/resident-archive/blob/master/functions/to-spotify/main.py#L398
-      "https://s3-eu-west-1.amazonaws.com/resident-archive/ra-stats.json",
-      (json) => {
-        // Embed Spotify track
-        const spotify_id = json['spotify_last_uri'].replace('spotify:track:', '');
-        document.getElementById("last-song").src = "https://open.spotify.com/embed/track/" + spotify_id;
+    loadData: () => {
+        $.getJSON(
+            // Generated in
+            // https://github.com/stephanebruckert/resident-archive/blob/master/functions/to-spotify/main.py#L398
+            "https://s3-eu-west-1.amazonaws.com/resident-archive/ra-stats.json",
+            (json) => {
+            // Embed Spotify track
+            const spotify_id = json['spotify_last_uri'].replace('spotify:track:', '');
+            document.getElementById("last-song").src = "https://open.spotify.com/embed/track/" + spotify_id;
 
-        // Numbers
-        document.getElementById("total-ra-songs").innerHTML = json['total_ra_songs'].toLocaleString();
-        document.getElementById("total-spotify-songs").innerHTML = json['total_spotify_songs'].toLocaleString();
-        document.getElementById("total-playlists").innerHTML = json['total_playlists'];
-        document.getElementById("ratio-ra-spotify").innerHTML = json['ratio_ra_spotify'];
+            // Numbers
+            document.getElementById("total-ra-songs").innerHTML = json['total_ra_songs'].toLocaleString();
+            document.getElementById("total-spotify-songs").innerHTML = json['total_spotify_songs'].toLocaleString();
+            document.getElementById("total-playlists").innerHTML = json['total_playlists'];
+            document.getElementById("ratio-ra-spotify").innerHTML = json['ratio_ra_spotify'];
 
-        // Time
-        const time_ago = moment.unix(json['spotify_last_find_time']).fromNow();
-        document.getElementById("spotify-last-find-time").innerHTML = time_ago;
+            // Time
+            const time_ago = moment.unix(json['spotify_last_find_time']).fromNow();
+            document.getElementById("spotify-last-find-time").innerHTML = time_ago;
 
-        // Update meta description
-        document.head.querySelector("[name~=description][content]").content += `
-          ${json['total_spotify_songs'].toLocaleString()}/${json['total_ra_songs'].toLocaleString()} songs added to ${json['total_playlists']} playlists.`;
-      }
-    );
-	}
+            // Update meta description
+            document.head.querySelector("[name~=description][content]").content += `
+                ${json['total_spotify_songs'].slice(0, -3)}k songs in ${json['total_playlists']} playlists.`;
+            }
+        );
+    }
 }
 
 $(function() { o.loadData(); });
